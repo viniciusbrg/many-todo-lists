225ddf1523580d97c4b865c79b35ac3fcf6b0386
Vinicius Bernardo <25652392+bgvinicius@users.noreply.github.com>
2020-01-27 21:03:45 -0300

    [wip] add frontend prototype

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,88 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello World
-      </h1>      
+    <div className="app-container">
+        <div className="sidebar">
+            <h1>Categorias</h1>
+            <h2>Default</h2>
+            <h2>Escola</h2>
+            <h2>Estudos</h2>
+            <h2>Faculdade</h2>
+            <h2>Comidas</h2>
+        </div>
+        <div className="main-content">
+            <div className="create-todo-bar">
+                <input type="text"></input>
+                <button>+</button>
+            </div>
+            <div className="todo-list">
+                <h1 className="todo-category">Comidas</h1>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+                <section className="todo-item">
+                    <h1>Todo 1</h1>
+                    <div className="todo-item-buttons">
+                        <span>Editar...</span>
+                        <span>Deletar</span>
+                    </div>
+                </section>
+
+            </div>
+        </div>
     </div>
   );
 }
