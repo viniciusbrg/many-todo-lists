ddaaa7219cd3a7371f9e8db5e5a4493577ad589b
Vinicius Bernardo <25652392+bgvinicius@users.noreply.github.com>
2020-01-29 15:09:10 -0300

    [javascript/tests] add todos functional tests

--- javascript/test/functional/todos.spec.js
+++ javascript/test/functional/todos.spec.js
@@ -6,7 +6,7 @@ const Todo = use('App/Models/Todo')
 trait('Test/ApiClient')
 trait('DatabaseTransactions')
 
-test('it should be able to list all todos on the database', async ({ assert, client }) => {
+test('it should be able to list all todos on the database', async ({ client }) => {
   const todoData = {
     name: 'My new todo',
     description: 'My todo description',
@@ -18,4 +18,80 @@ test('it should be able to list all todos on the database', async ({ assert, cli
 
   response.assertStatus(200)
   response.assertJSONSubset([todoData])
+})
+
+test('it should be able to list a specific todo into the database', async ({ client }) => {
+  const todoData = {
+    name: 'My new todo',
+    description: 'My todo description',
+  }
+
+  await Todo.create(todoData)
+
+  const response = await client.get('/todos/1').end()
+
+  response.assertStatus(200)
+  response.assertJSONSubset(todoData)
+})
+
+test('it should be able to create a new todo on the database', async ({ client, assert }) => {
+  const todoData = {
+    name: 'My new todo',
+    description: 'My todo description',
+  }
+
+  const response = await client.post('/todos').send(todoData).end()
+
+  response.assertStatus(201)
+
+  const createdTodo = response.body
+
+  const todoInDatabase = await Todo.find(createdTodo.id)
+
+  assert.deepEqual(todoInDatabase.toJSON(), createdTodo)
+})
+
+test('it should be able to edit existing todos on the database', async ({ client, assert}) => {
+  const todoData = {
+    name: 'My new todo',
+    description: 'My todo description',
+    id: 1
+  }
+
+  await Todo.create(todoData)
+
+  const dataToUpdate = {
+    name: 'My updated todo',
+    description: 'My updated description',
+  }
+
+  const response = await client.put('/todos/1').send(dataToUpdate).end()
+
+  response.assertStatus(200)
+
+  const updatedTodo = response.body
+
+  const todoInDatabase = await Todo.find(todoData.id)
+  const todoJSON = todoInDatabase.toJSON()
+
+  assert.deepEqual(todoJSON, updatedTodo)
+  assert.include(todoJSON, dataToUpdate)
+})
+
+test('it should be able to delete an existing todo', async ({ client, assert }) => {
+  const todoData = {
+    name: 'My new todo',
+    description: 'My todo description',
+    id: 1
+  }
+
+  await Todo.create(todoData)
+
+  const response = await client.delete('/todos/1').end()
+
+  response.assertStatus(204)
+
+  const todo = await Todo.find(todoData.id)
+
+  assert.isNull(todo, 'the todo wasnt deleted from the database!');
 })
