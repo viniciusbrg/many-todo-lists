0a9204636415487bed917150960f09d4e63f2719
Vinicius Bernardo <25652392+bgvinicius@users.noreply.github.com>
2020-02-05 15:19:41 -0300

    [frontend] separate components in different folders

--- frontend/src/components/TodoList/Main.js
+++ frontend/src/components/TodoList/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import TodoItem from './TodoItem'
-import api from '../services/api'
+import api from '../../services/api'
 
 function Main() {
   const [todos, setTodos] = useState([])
